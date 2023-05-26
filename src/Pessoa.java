95dc0089428f8bdc32d4eeaa05811e28aef5c343
rm87150 <decounger0510@gmail.com>
2023-05-26 09:55:58 -0300

    ok

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,12 +1,21 @@
-public class Pessoa {
+public class Pessoa extends Animal {
     // Atributo
-    String nome;
+
     String sobrenome;
     // Métodos da classe
     public void falar(){
         System.out.println("falei");
     }
+    // Sobreacarga de método
     public void falar(String volume){
         System.out.println("falei" + volume);
     }
+    public String falar(String volume, String tom){
+        return "falei " + volume;
+    }
+    // Sobrescrita de método
+    public void comer(){
+        System.out.println("Comer como pessoa");
+    }
+
 }
